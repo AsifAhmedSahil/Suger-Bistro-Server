30452fa9b55ffa409db89c8a989eed0b90c83bcd
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-03-10 02:12:01 +0600

    Display All Users And Create Make Admin API

--- index.js
+++ index.js
@@ -68,6 +68,18 @@ async function run() {
       res.send(result);
     })
 
+    app.patch("/users/admin/:id",async(req,res) =>{
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const updatedDoc ={
+        $set:{
+          role:'admin'
+        }
+      }
+      const result = await userCollection.updateOne(filter,updatedDoc);
+      res.send(result);
+    })
+
     // cart collection
 
     app.get("/carts" , async(req,res)=>{
