0718e9a8551113d112e958c65e5a1754aa14a80b
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-03-10 12:21:22 +0600

    Create A JWT Token And Save It On Local Storage

--- index.js
+++ index.js
@@ -44,9 +44,31 @@ async function run() {
       res.send(result)
     })
 
+    // jwt api created
+    app.post('/jwt' , async (req,res) =>{
+      const user = req.body;
+      const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn: '1h'});
+      res.send({token})
+    })
+
+
+    // Middleware verify token
+
+
+    const verifyToken = (req,res,next) =>{
+        console.log('inside verify token',req.headers);
+        // next()
+        if(!req.headers.authorization){
+            return res.status(401).send({message: 'forbidden access'})
+        }
+        const token = req.headers.authorization.split(" ")[1]
+    }
+
+
     // User Collection
 
-    app.get("/users" ,async(req,res) =>{
+    app.get("/users",verifyToken ,async(req,res) =>{
+      console.log(req.headers)
       const result = await userCollection.find().toArray();
       res.send(result)
     })
