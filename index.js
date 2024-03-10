de066ded72110a9e25370e235269f51c6a1b3be0
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-03-10 12:56:10 +0600

    Verify Token And Axios Interceptor

--- index.js
+++ index.js
@@ -56,12 +56,20 @@ async function run() {
 
 
     const verifyToken = (req,res,next) =>{
-        console.log('inside verify token',req.headers);
+        console.log('inside verify token',req.headers.authorization);
         // next()
         if(!req.headers.authorization){
             return res.status(401).send({message: 'forbidden access'})
         }
         const token = req.headers.authorization.split(" ")[1]
+
+        jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded) =>{
+          if(err){
+            return res.status(401).send({message: 'forbidden access'})
+          }
+          res.decoded = decoded
+          next()
+        })
     }
 
 
